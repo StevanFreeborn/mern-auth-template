f74169523ed37f07f4854137e6dd84cfcea5ec7a
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-27 09:51:31 -0500

    chore: setup prettier and eslint

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,18 @@
 import 'dotenv/config';
 import express from 'express';
+import fs from 'fs';
 import swaggerUi from 'swagger-ui-express';
 import { env } from '../env.js';
 import { error, notFound } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
-import swaggerJson from './swagger/swagger.json' assert { type: 'json' };
 
 const app = express();
 
 app.use('/api/users', userRoutes);
 
 if (env.NODE_ENV === 'development') {
+  const swaggerJsonString = fs.readFileSync('./swagger/swagger.json', 'utf-8');
+  const swaggerJson = JSON.parse(swaggerJsonString);
   app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 }
 
